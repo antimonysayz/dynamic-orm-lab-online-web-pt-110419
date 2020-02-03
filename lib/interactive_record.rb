3cd32c5cb8c423cacb5a81e09d4f7f9d8e71a167
Anthony Mai <anthony.mai530@gmail.com>
2020-02-03 02:12:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,9 +2,18 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = <<-SQL
+    PRAGMA table_info('{table_name}')
+    SQL
+    table_info = DB[:conn].execute(sql)
+
 
 
 end
