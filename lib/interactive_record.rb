5ce9b3c286f048b6416580069460cfc2bf6dfe00
Anthony Mai <anthony.mai530@gmail.com>
2020-02-03 02:28:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,5 +59,8 @@ def self.find_by_name(name)
   DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name=?", name)
 end
 
+def self.find_by(hash)
+  sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = #{hash.values}[0].to_s"
+
 
 end
