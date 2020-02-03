d29bca01b28ded739a9dfa07e33c11d1ef536d11
Anthony Mai <anthony.mai530@gmail.com>
2020-02-03 02:22:01 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -33,6 +33,7 @@ def save
   DB[:conn].execute(sql)
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
 end
+
 def table_name_for_insert
   names = []
   self.class.table_name
